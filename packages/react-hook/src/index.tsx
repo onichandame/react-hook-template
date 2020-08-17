f5587ded9ae80beaa89f51e40632a845ad88daab
onichandame <zxinmyth@gmail.com>
2020-08-17 15:36:01 +0000

    done init

--- packages/react-hook/src/index.tsx
+++ packages/react-hook/src/index.tsx
@@ -1,11 +1,9 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import { useReducer, useEffect } from 'react'
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  children?: ReactChild;
+export const useCount = () => {
+  const [val, update] = useReducer((old) => old + 1, 0)
+  useEffect(() => {
+    setInterval(update, 1000)
+  }, [])
+  return { val }
 }
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
-};
