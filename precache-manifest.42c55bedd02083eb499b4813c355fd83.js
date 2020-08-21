94af478ea48f2d4f8f7231e94f2a953d16a6f215
onichandame <zxinmyth@gmail.com>
2020-08-21 06:00:29 +0000

    Deploying to gh-pages from  @ 7a49914b6c2c2f3be6cbc07d46f1704d6bd90343 🚀

---
+++ precache-manifest.42c55bedd02083eb499b4813c355fd83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14bdd1fa22255ffa3dceb8d18e589150",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2bbc7604eeb39e4e4804",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6711b79880b62ebeabea",
+    "url": "./static/js/2.f5498493.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f5498493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbc7604eeb39e4e4804",
+    "url": "./static/js/main.89356fb4.chunk.js"
+  },
+  {
+    "revision": "b12fb06c1b19e2084157",
+    "url": "./static/js/runtime-main.e89f0be9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb67737d77223dc3fdf88e31df20a4a7.js"
+  "./precache-manifest.42c55bedd02083eb499b4813c355fd83.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
